af28fce503982d1e73311b71b59f6b3f9577f4e6
JcDieguez <juancruzdieguez95@gmail.com>
2022-11-29 22:58:53 -0300

    Motor plantilla EJS

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+//Middlewares
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+//view engine
+
+app.set("views", "./views/ejs");
+app.set("view engine", "ejs");
+
+
+let productos = [];
+
+app.get("/", (req, res, next)=>{
+    // res.send("Ok")
+    res.render("index", {productos});
+});
+
+app.post("/productos", (req, res, next)=>{
+    productos.push(req.body);
+    res.redirect("/");
+});
+
+
+
+app.listen(PORT);
